5a59a5a0b303fd9a44a413ae9bb87156dc001827
Steven Tyson <steventyson@Stevens-MBP.lan>
2019-05-25 11:08:47 +1000

    new func

--- 28-testing/01/main.go
+++ 28-testing/01/main.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/steveyttt/go/28-testing/01/acdc"
 	"github.com/steveyttt/go/28-testing/01/metallica"
+	"github.com/steveyttt/go/28-testing/01/soad"
 )
 
 func main() {
 
 	fmt.Println("the numbers add up to", acdc.Sum(1, 2, 3, 4))
 	fmt.Println("the numbers multiply to be", metallica.Mymulti(2, 2, 2))
+	fmt.Println("The numbers divided equal", soad.MyDivision(4, 2))
 
 }
---
+++ 28-testing/01/proach/main.go
@@ -0,0 +1,7 @@
+package proach
+
+//MyMinus is used to minus one number from another
+func MyMinus(x int, y int) int {
+	s := x - y
+	return s
+}
